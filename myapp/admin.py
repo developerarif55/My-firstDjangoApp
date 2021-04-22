9def514127227c37f1ddee3258a7af6c661d49dd
Md ARIF HOSSAIN <67253953+developerarif55@users.noreply.github.com>
2021-04-22 07:28:20 +0600

    initial commit

---
+++ myapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Profile
+
+# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display= [
+        'name',
+        'age',
+        'address',
+        'image'
+    ]
+admin.site.register(Profile, ProfileAdmin)    
---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Profile
+
+
+# Create your views here.
+def homepage(request):
+
+    profile = Profile.objects.all()
+
+    context ={
+        'profile':profile
+
+    }
+
+    return render(request, 'index.html', context)
