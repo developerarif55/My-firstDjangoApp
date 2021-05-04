c2131a09bf546f769ed0e2ea078336af1e14c93f
developerarif55 <arifma0947@gmail.com>
2021-05-04 15:21:16 +0600

    once agin for css

--- test2/urls.py
+++ test2/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls', namespace='myapp')),
     path('album/', include('album.urls', namespace='album'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
